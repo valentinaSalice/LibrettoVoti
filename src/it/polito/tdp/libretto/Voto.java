b904a0e4208933671abb8430af6d9469d66100db
Valentina <Valentina@172.21.56.70>
2019-03-11 13:52:52 +0100

    Progetto iniziale. Classe Voto vuota.

---
+++ src/it/polito/tdp/libretto/Voto.java
@@ -0,0 +1,45 @@
+package it.polito.tdp.libretto;
+
+import java.time.LocalDate;
+
+public class Voto {
+	
+	private int voto;
+	private String corso;
+	private LocalDate data; //Date o Calendar permettono di gestire le date, creavano dei problemi. Da Java 7 è stato introdotto LocalDate
+	
+	public Voto(int voto, String corso, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.corso = corso;
+		this.data = data;
+	}
+
+	public int getVoto() {
+		return voto;
+	}
+
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+
+	public String getCorso() {
+		return corso;
+	}
+
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+
+	
+}
