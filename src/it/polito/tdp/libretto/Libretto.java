aa783c577c5d8d3cd0b699770d78c1f6156c63ce
Valentina <Valentina@Valentina-PC>
2019-03-18 13:36:02 +0100

    Implementato il punto 1

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,30 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
+	
+	private List <Voto>voti; //conviene mantere le interfacce più generali possibili e poi specificare nel costruttore
+	
+	public Libretto() {
+		
+		this.voti=new ArrayList<Voto>();
+	}
+	
+	public void add(int voto, String corso, LocalDate data) {
+		
+		
+	}
+	
+	/**
+	 * Aggiunge un nuovo voto al libretto
+	 * @param v {@link Voto} da aggiungere
+	 */
+	public void add(Voto v) {
+		
+		voti.add(v);
+	}
 
 }
--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -38,6 +38,14 @@ public class Voto {
 	public void setData(LocalDate data) {
 		this.data = data;
 	}
+
+	@Override
+	public String toString() {
+		return String.format("Voto [voto=%s, corso=%s, data=%s]", voto, corso, data);
+	}
+	
+	
+	
 	
 	
 
