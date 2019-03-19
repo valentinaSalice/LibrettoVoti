98a91dd141fa180c66a09a643b61df0eaf4c5167
Valentina <Valentina@Valentina-PC>
2019-03-19 11:39:33 +0100

    Implementato esercizio 4

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -67,5 +67,29 @@ public class Libretto {
 		
 		return null;
 	}
+	
+	/**
+	 * Dato un {@link Voto}, determina se esiste già un voto con uguale corso e uguale punteggio
+	 * @param v
+	 * @return {@code true} se ha trovato un corso e punteggio uguali, {@code false} se non ha trovato il corso, oppure l'ha trovato con voto diverso
+	 */
+	
+	public boolean esisteGiaVoto(Voto v) {
+		
+		Voto trovato=this.cercaEsame(v.getCorso());
+		
+		if(trovato==null)
+			return false;
+		if(trovato.getPunti()==v.getPunti()) {
+			
+			return true;
+		}
+		else
+			return false;
+		
+		
+		
+		
+	}
 
 }
